f464ad981995250aabdd945144685b91f9d29d19
AdjWang <wlstemp@163.com>
2023-05-09 01:57:49 +0000

    update pseudo code

--- pseudo.py
+++ pseudo.py
@@ -1,22 +1,23 @@
 def invoke(callee, input):
     tagPre = hashLogTag([ID, STEP, "pre"])
     logAppend(tags: [tagPre],
-              data: { "calleeId": UUID() })
+              data: {"calleeId": UUID()})
     rec = logReadNext(tag: tagPre, minSeqnum: 0)
     calleeId = rec.data["calleeId"]
     retVal = rawInvoke(callee, [calleeId, input])
     tagPost = hashLogTag([ID, STEP, "post"])
     logAppend(tags: [tagPost],
-              data: { "retVal": retVal })
+              data: {"retVal": retVal})
     rec = logReadNext(tag: tagPost, minSeqnum: 0)
     STEP = STEP + 1
     return rec.data["retVal"]
+
 
 def invoke(callee, input):
     STEP = STEP + 1
     tagInvoke = hashLogTag([ID, STEP, "pre"])
     logAppend(tags: [tagInvoke],
-              data: { "calleeId": UUID() })
+              data: {"calleeId": UUID()})
     rec = logReadNext(tag: tagInvoke, minSeqnum: 0)
     if rec != None:
         return rec.data["retVal"]
@@ -27,51 +28,40 @@ def invoke(callee, input):
 
     return retVal
 
-def rawInvokeWrapper(ctx, user_func):
-    logAppend(tags: [IntentLogTag],
-              data: { "instanceId": ctx.callee.ID })
 
-    retVal = user_func(ctx.callee.Input)
 
-    if ctx.caller != None:
-        tagInvoke = hashLogTag([ctx.caller.ID, ctx.caller.STEP, "callback"])
-        logAppend(tags: [tagInvoke]
-                  data: { "result": retVal })
 
-    logAppend(tags: [IntentLogTag],
-              data: { "Done": True })
 
-    return retVal
-
-def invoke(callee, input):
+def write(ctx, table, key, val):
     STEP = STEP + 1
-    tagInvoke = hashLogTag([ID, STEP, "pre"])
-    lastStep = asyncLogAppend(tags: [tagInvoke],
-                              data: { "calleeId": UUID() })
-               .chain(asyncLogReadNext(tag: tagInvoke, minSeqnum: 0))
+    tag = hashLogTag([ID, STEP, "pre"])
+    lastStepPre = asyncLogAppend(tags: [tag],
+                                 data: [table, key, val],
+                                 dep: ctx.logs.lastStep,
+                                 cond: Cond_IsTheFirstStep)
+    ctx.logs.chain(lastStepPre)
+    # await and resolve dependencies and conditions
+    ctx.logs.sync()
+    rec = logReadNext(tag: tag, minSeqnum: 0)
+    rawDBWrite(table, key,
+               cond: "Version < {rec.seqnum}",
+               update: "Value={val}; Version={rec.seqnum}")
+    tag = hashLogTag([ID, STEP, "post"])
+    lastStepPost = asyncLogAppend(tags: [tag],
+                                  data: [table, key, val],
+                                  dep: lastStepPre)
+    ctx.logs.chain(lastStepPost)
 
+
+def invoke(ctx, callee, input):
+    STEP = STEP + 1
+    tag = hashLogTag([ID, STEP])
     calleeId = UUID()
-    ctx = Context(NewContext(), calleeId, input, lastStep)
-    retVal = rawInvoke(ctx, callee)
-
-    return retVal
-
-def rawInvokeWrapper(ctx, user_func):
-    lastStep = ctx.lastStep
-    lastStep.chain(asyncLogAppend(tags: [IntentLogTag],
-                                  data: { "instanceId": ctx.calleeID }))
-    if await lastStep.verify():
-        return lastStep.unwrap().data["retVal"]
-
-    retVal = user_func(ctx.calleeInput)
-
-    if ctx.caller != None:
-        tagInvoke = hashLogTag([ctx.callerID, ctx.callerSTEP, "callback"])
-        lastStep.chain(asyncLogAppend(tags: [tagInvoke]
-                                      data: { "result": retVal }))
-
-    lastStep.chain(asyncLogAppend(tags: [IntentLogTag],
-                                  data: { "Done": True }))
-    ctx.Update(lastStep)
-
+    lastStep = asyncLogAppend(tags: [tag],
+                              data: {"calleeId": calleeId},
+                              dep: ctx.logs.lastStep,
+                              cond: Cond_IsTheFirstStep)
+    ctx.logs.chain(lastStep)
+    calleeId = rec.data["calleeId"]
+    retVal = rawInvoke(ctx, callee, [ctx, calleeId, input])
     return retVal
