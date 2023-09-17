f62c67ef9f9aa158e142a5ccea074c8fa47db99b
adjwang <wwang230513@gmail.com>
2023-09-17 07:53:44 +0000

    add e2f batch but not so effective

--- experiments/run_quick.sh
+++ experiments/run_quick.sh
@@ -4,10 +4,10 @@ set -uxo pipefail
 ROOT_DIR=`realpath $(dirname $0)/..`
 
 # Microbenchmarks
-RUN_MICROBENCH=y
+RUN_MICROBENCH=
 
 # Message queue workload for BokiQueue and Pulsar
-RUN_QUEUE_BOKI=y
+RUN_QUEUE_BOKI=
 RUN_QUEUE_PUSLAR=
 RUN_QUEUE_SQS=
 
