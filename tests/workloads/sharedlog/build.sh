1200efcfa5f4f452d3d85125d92c3a551bf6aa54
adjwang <wwang230513@gmail.com>
2023-10-10 05:25:36 +0000

    separate shared index as distinct module

--- tests/workloads/sharedlog/build.sh
+++ tests/workloads/sharedlog/build.sh
@@ -7,6 +7,8 @@ TESTCASE_DIR=$(realpath $PROJECT_DIR/tests/workloads/sharedlog)
 WORKFLOW_DIR=$(realpath $PROJECT_DIR/workloads/workflow)
 
 export CGO_ENABLED=1
+export CGO_CFLAGS="$(go env CGO_CFLAGS) -I$BOKI_DIR/lib/shared_index/include"
+export CGO_LDFLAGS="$(go env CGO_LDFLAGS) -L$BOKI_DIR/lib/shared_index/bin/debug -lrt -ldl -lindex"
 
 cd $TESTCASE_DIR
 go mod edit -replace=cs.utexas.edu/zjia/faas=$BOKI_DIR/worker/golang
