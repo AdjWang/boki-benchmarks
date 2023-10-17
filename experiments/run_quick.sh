93af30adb1eb6dda7f04cd40a7108dd60234f889
adjwang <wwang230513@gmail.com>
2023-10-17 09:12:25 +0000

    analysis TxnCommit read performance

--- experiments/run_quick.sh
+++ experiments/run_quick.sh
@@ -140,7 +140,7 @@ $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR --instance-iam-role $BOKI_MAC
 $BASE_DIR/run_once.sh con192 192
 $BASE_DIR/run_once.sh con2048 2048
 
-$HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
+# $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
 echo "[DEBUG] early exit"
 exit 0
 
